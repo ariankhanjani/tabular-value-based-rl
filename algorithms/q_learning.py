d59fae7fa733c1bfaa759e88410c4ce03fa28156
arian <ariankhanjani79@gmail.com>
2026-02-21 23:32:45 +0330

    Add SARSA and Q_learning algorithms.

---
+++ algorithms/q_learning.py
@@ -0,0 +1,51 @@
+import numpy as np
+from collections import defaultdict
+
+
+def Q_learning(env, num_episodes=10000, alpha=0.3, gamma=0.98, epsilon=0.2):
+    """Q-Learning (off-policy TD control) algorithm.
+
+    Args:
+        env (gym.Env): Gymnasium-compatible environment.
+        num_episodes (int, optional): Number of training episodes.
+        alpha (float, optional): Learning rate.
+        gamma (float, optional): Discount factor.
+        epsilon (float, optional): Exploration rate.
+
+    Returns:
+        tuple:
+            Q (dict): Learned Q-table.
+            rewards (list): Episode reward history.
+    """
+    
+    num_actions = env.action_space.n
+    Q = defaultdict(lambda: np.zeros(num_actions))
+    rewards = []
+
+    for _ in range(num_episodes):
+        state, _ = env.reset()
+        state = int(state)
+
+        done = False
+        episode_reward = 0
+
+        while not done:
+            action = policy(Q, state, num_actions, epsilon)
+            next_state, reward, done, truncated, _ = env.step(action)
+            next_state = int(next_state)
+
+            best_next = np.max(Q[next_state])
+            td_target = reward + gamma * best_next * (not done)
+            td_error = td_target - Q[state][action]
+
+            Q[state][action] += alpha * td_error
+
+            state = next_state
+            episode_reward += reward
+
+            if truncated:
+                break
+
+        rewards.append(episode_reward)
+
+    return Q, rewards
